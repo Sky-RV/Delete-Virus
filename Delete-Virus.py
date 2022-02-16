fbbc174dd417b2112ffa8dc0580c3f869dec06dc
Sky <90393918+Sky-RV@users.noreply.github.com>
2022-02-17 02:12:03 +0330

    Delete Custom Files

--- Delete-Virus.py
+++ Delete-Virus.py
@@ -1,26 +1,66 @@
 from ast import For
+from asyncio.windows_events import NULL
 import os
 import time
 from colorama import Fore, Style, init
+from datetime import datetime, timedelta
 
 ############################## DELETE FILE ##############################
 
 def deleteFiles(name):
 
-    time.sleep(3)
-
-    for i in os.listdir():
-        if name in i and i != 'Delete-Virus.py':
-            os.remove(i)
-            print(Fore.LIGHTRED_EX + " File Deleted Successfully.\n" + Style.RESET_ALL)
+    if name == NULL:
+        
+        os.system('cls')
+        
+        print(Fore.WHITE + ' Enter your target ' + Fore.LIGHTBLACK_EX + '(like .txt) : ')
+        custom = input(Fore.GREEN + ' >> ' + Style.RESET_ALL)
+        
+        if custom[0] == '.':
+            time.sleep(2)
+        
+            for i in os.listdir():
+                if custom in i and i != 'Delete-Virus.py':
+                    os.remove(i)
+                    print(Fore.LIGHTRED_EX + "\n File Deleted Successfully.\n" + Style.RESET_ALL)
+                    
+                    END()
+        
+            print(Fore.RED + '\n File doesn''t found. \n')
             
-    END()
+            END()
+            
+        else:
+            print(Fore.RED + ' Format was wrong. Please try again...\n')
+            
+            print(Fore.LIGHTBLACK_EX + " Back to main in 5 seconds...")
+            
+            total_seconds = 5
+            
+            while total_seconds > 0:
+                timer = timedelta(seconds = total_seconds)
+                print(" " ,timer, end="\r")
+                time.sleep(1)
+                total_seconds -= 1
+            main()
+    
+    else:
+        
+        os.system('cls')
+        
+        time.sleep(2)
+        
+        for i in os.listdir():
+            if name in i and i != 'Delete-Virus.py':
+                os.remove(i)
+                print(Fore.LIGHTRED_EX + "\n File Deleted Successfully.\n" + Style.RESET_ALL)
+        END()
 
 ############################## END ##############################
 
 def END():
-    print(Fore.RED + ' [1] ' + Style.RESET_ALL + 'Back to main\n' +
-          Fore.LIGHTBLACK_EX + 'Press any key to exit...'
+    print(Fore.RED + ' [1] ' + Style.RESET_ALL + 'Back to main\n\n' +
+          Fore.LIGHTBLACK_EX + ' Press any key to exit...\n'
           )
     
     answer = input(Fore.GREEN + ' >> ' + Style.RESET_ALL)
@@ -41,8 +81,8 @@ def main():
         Fore.RED + ' [2] ' + Style.RESET_ALL + 'Detele .mp4 Files\n' +
         Fore.RED + ' [3] ' + Style.RESET_ALL + 'Detele .mp3 Files\n' +
         Fore.RED + ' [4] ' + Style.RESET_ALL + 'Detele .pdf Files\n' +
-        Fore.RED + ' [6] ' + Style.RESET_ALL + 'Detele Custom Files\n' +
-        Fore.RED + ' [7] ' + Style.RESET_ALL + 'Detele All Files\n\n' +
+        Fore.RED + ' [5] ' + Style.RESET_ALL + 'Detele Custom Files\n' +
+        Fore.RED + ' [6] ' + Style.RESET_ALL + 'Detele All Files\n\n' +
         Fore.LIGHTBLACK_EX + " Press any key to exit...\n"
     )
 
@@ -57,10 +97,8 @@ def main():
     elif answer == '4':
         deleteFiles('.pdf')
     elif answer == '5':
-        pass
+        deleteFiles(name=NULL)
     elif answer == '6':
-        pass
-    elif answer == '7':
         pass
     else:
         pass
